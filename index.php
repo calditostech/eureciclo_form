45626b4e65b72967b90b156afb6a9273cc77be29
Ricardo Ferreira <calditos1988@outlook.com>
2022-02-13 12:18:36 -0300

    desenvolvendo o front

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
 session_start(); //Inicializar a sessão com PHP
+
+//Incluir a conexão com o banco de dados
+include_once("conexao.php");
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
 		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+		<link rel="stylesheet" type="text/css" href="styles.css" media="screen" />
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 		<title>Importar TXT</title>
 	</head>
 	<body>
 		<!--Titulo do Formulário-->
-		<h1>Importar dados do arquivo TXT</h1>
+		<h1 class="titulo">Importar dados do arquivo TXT</h1>
 		<?php
 		//Imprimir a mensagem de sucesso no upload de dados do arquivo txt para o banco de dados com mysqli
 		if(isset($_SESSION['msg'])){
@@ -21,11 +26,47 @@ session_start(); //Inicializar a sessão com PHP
 		}
 		?>
 		<!--Formulário com PHP para fazer upload de arquivo com PHP-->
-		<form method="POST" action="processa.php" enctype="multipart/form-data">
+		<form method="POST" action="processa.php" enctype="multipart/form-data" class="form">
 			<label>Arquivo</label>
 			<!--Campo para fazer o upload do arquivo com PHP-->
-			<input type="file" name="arquivo"><br><br>			
+			<input type="file" name="arquivo" class="arquivo-btn"><br><br>			
 			<input type="submit" value="Importar">
 		</form>
+
+		<!-- tabela -->
+		<table class="table">
+			<thead>
+				<tr>
+				<th scope="col">COMPRADOR</th>
+				<th scope="col">DESCRIÇÃO</th>
+				<th scope="col">PREÇO</th>
+				<th scope="col">QUANTIDADE</th>
+				<th scope="col">ENDEREÇO</th>
+				<th scope="col">FORNECEDOR</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+				<?php
+				//Criar a QUERY para pesquisar os produtos no banco de dados
+				$result_usuario = "SELECT * FROM vendas";
+				//Executar a QUERY para pesquisar os produtos no banco de dados com o MySQLi
+				$resultado_usuario = mysqli_query($conn, $result_usuario);
+				
+				//Usar o while para percorrer os dados trazidos do banco de dados
+				while($row_vendas = mysqli_fetch_assoc($resultado_usuario)){
+				?>
+				<td><?php echo $row_vendas['comprador']; ?></td>
+				<td><?php echo $row_vendas['descricao']; ?></td>
+				<td><?php echo $row_vendas['preco']; ?></td>
+				<td><?php echo $row_vendas['quantidade']; ?></td>
+				<td><?php echo $row_vendas['endereco']; ?></td>
+				<td><?php echo $row_vendas['fornecedor']; ?></td>
+				<?php
+				  }
+				?>
+				</tr>
+			</tbody>
+       </table>
 	</body>
 </html>
--- processa.php
+++ processa.php
@@ -24,14 +24,20 @@ foreach($dados as $linha){
 	$quantidade = $valor[3];
 	$endereco = $valor[4];
     $fornecedor = $valor[5];
-	
 	//Criar a QUERY com PHP para inserir os dados no banco de dados
 	$result_usuario = "INSERT INTO vendas (comprador, descricao, preco, quantidade, endereco, fornecedor) VALUES ('$comprador', '$descricao', '$preco', '$quantidade', '$endereco', '$fornecedor')";
-	file_put_contents("teste30.txt", $result_usuario);
 	//Executar a QUERY para inserir os registros no banco de dados com MySQLi
-	$resultado_usuario = mysqli_query($conn, $result_usuario);	
+	$resultado_usuario = mysqli_query($conn, $result_usuario);
+	// transforma os dados em um array
+	$executa = mysqli_fetch_assoc($resultado_usuario);
+	// calcula quantos dados retornaram
+    $total = mysqli_num_rows($resultado_usuario);
 }
 //Criar a variável global com a mensagem de sucesso
+if($resultado_usuario){
 $_SESSION['msg'] = "<p style='color: green;'>Carregado os dados com sucesso!</p>";
+} else {
+$_SESSION['msg'] = "<p style='color: red;'>Dados não enviado!</p>";	
+}
 //Redirecionar o usuário com PHP para a página index.php
 header("Location: index.php");
